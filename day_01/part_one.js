a1caca1f0c469e1efdc9887299a7a1ee712dabfe
Douya <1002412798@qq.com>
2022-12-09 19:09:58 +0800

    Update part_one.js

--- day_01/part_one.js
+++ day_01/part_one.js
@@ -1,3 +1,4 @@
+
 const path = require('path');
 const fs = require('fs');
 
@@ -8,4 +9,24 @@ const input = fs
 	.split('\n\n');
 
 // Make your solution!
-console.log(input)
+// console.log(input);
+var inputArr = input.map(function(x){  //以换行符分隔成数组中的数组
+	return x.split('\n');
+});
+// console.log(inputArr);
+
+
+var maxSum = 0;
+for (i=0 ; i<inputArr.length ; i++){
+	let arrSum = 0;
+	inputArr[i].forEach(function(x){
+		arrSum = arrSum + Number(x);
+	})
+	if (arrSum > maxSum) {
+		maxSum = arrSum;
+	}
+}
+console.log(maxSum);
+
+
+
